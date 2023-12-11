68956e4224a372b98bcd3b2796e04e4396b5ea4b
João Lourenço <jrelourenco@outlook.pt>
2023-12-11 12:13:37 +0000

    fix: export sleep entrypoint (#9)
    * test: checks if retry is exported by the library
    
    * test: checks if sleep is exported by the library
    
    * fix: export sleep entrypoint

---
+++ tests/entrypoints.test.ts
@@ -0,0 +1,24 @@
+import { expect, describe, it, vi, beforeAll } from "vitest";
+
+describe("library entrypoints", () => {
+  it("should export retry entrypoint", async () => {
+    const { retry } = await getLib();
+    const fn = vi.fn().mockImplementationOnce(() => ({ success: true }));
+
+    expect(await retry(fn)).toEqual({ success: true });
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it("should export sleep entrypoint", async () => {
+    const { sleep } = await getLib();
+
+    const start = Date.now();
+    await sleep(1000);
+    const end = Date.now();
+    expect(end - start).toBeGreaterThanOrEqual(1000);
+  });
+});
+
+async function getLib() {
+  return await import("./../src");
+}
