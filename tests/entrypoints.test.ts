f3c045efb940c178ad0b988a8b99433e435f6f10
João Lourenço <jrelourenco@outlook.pt>
2023-12-11 11:08:50 +0000

    test: checks if sleep is exported by the library

--- tests/entrypoints.test.ts
+++ tests/entrypoints.test.ts
@@ -8,6 +8,16 @@ describe("library entrypoints", () => {
     expect(await retry(fn)).toEqual({ success: true });
     expect(fn).toHaveBeenCalledTimes(1);
   });
+
+  it("should export sleep entrypoint", async () => {
+    // @ts-expect-error
+    const { sleep } = await getLib();
+
+    const start = Date.now();
+    await sleep(1000);
+    const end = Date.now();
+    expect(end - start).toBeGreaterThan(1000);
+  });
 });
 
 async function getLib() {
